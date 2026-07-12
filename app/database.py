023b4b32a56c4566854ab61ed1ebf932c43f7f1c
Jatin Koshti <jatinkoshti695@gmail.com>
2026-07-12 13:44:57 +0530

    Update database connection

--- app/database.py
+++ app/database.py
@@ -4,9 +4,18 @@ import os
 
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGODB_URI"))
+MONGODB_URI = os.getenv("MONGODB_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
 
-db = client[os.getenv("DATABASE_NAME")]
+if not MONGODB_URI:
+    raise Exception("MONGODB_URI environment variable not found")
+
+if not DATABASE_NAME:
+    raise Exception("DATABASE_NAME environment variable not found")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DATABASE_NAME]
 
 employees = db["employees"]
 
